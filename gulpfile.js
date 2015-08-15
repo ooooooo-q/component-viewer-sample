8d2eeceef9f3c9ac83737716419af56f30472164
ooooooo-q <ooooooo-q@users.noreply.github.com>
2015-08-16 02:18:41 +0900

    enable port

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var componentViewer = require('component-viewer');
 
 gulp.task('components', function() {
   componentViewer({
+    port: process.env.PORT || 3000,
     scenarios: './test/scenarios/**/*.js',
     assets: './lib',
 
