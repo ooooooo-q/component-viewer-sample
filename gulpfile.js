0d9541d261f1a9f4400e056d2cbdd188eb91085b
ooooooo-q <ooooooo-q@users.noreply.github.com>
2015-08-17 08:27:22 +0900

    optional port from cli arguments

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
+var argv = require('yargs').argv;
 
 var componentViewer = require('component-viewer');
 
 gulp.task('components', function() {
+  var port = argv.port || process.env.PORT || 3000;
+
   componentViewer({
-    port: process.env.PORT || 3000,
+    port: port,
     scenarios: './test/scenarios/**/*.js',
     assets: './lib',
 
